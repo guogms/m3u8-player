115c669d4e469c91a8c02d5b8f4b4153ecf02e56
Misin <guogms@qq.com>
2025-07-16 16:22:27 +0800

    Revert "修复"
    This reverts commit 86b0e15104990cd8ec6778bb0ae853c8661acb25.

--- app/api/send-email/route.ts
+++ app/api/send-email/route.ts
@@ -1,6 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
 import nodemailer from 'nodemailer';
-export const runtime = 'edge';
 
 export async function POST(req: NextRequest) {
   try {
