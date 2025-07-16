fe3ed81d3208dbe78a2554c0d87b752fd1279189
Misin <guogms@qq.com>
2025-07-16 22:17:57 +0800

    Revert "测试"
    This reverts commit 484df665dc24cf3128d6ad7f429d46b5971141de.

--- app/api/send-email/route.ts
+++ app/api/send-email/route.ts
@@ -135,9 +135,9 @@ export async function POST(req: NextRequest) {
           to                          // RCPT TO
         },
         headers: {
-          'from': 'guogms1022@163.com',
+          'from-': fromAddress,
           'to': toAddress,
-          'X-Original-From': 'guogms1022@163.com',
+          'X-Original-From': parsed.from?.text || formattedFrom,
           'X-Original-To': originalTo,
           'X-Original-CC': originalCC,
           'Reply-To': fromAddress
