25fc38f212dc2a4ab749c06390a1562f83d24660
Misin <guogms@qq.com>
2025-12-16 02:12:52 +0800

    Revert "功能：添加 Cloudflare Pages 部署说明及相关配置"
    This reverts commit fab545573e59e4564b7d2aaef74090bdd7c52eff.

--- next.config.mjs
+++ next.config.mjs
@@ -21,7 +21,6 @@ const nextConfig = {
     parallelServerBuildTraces: true,
     parallelServerCompiles: true,
   },
-  // Cloudflare Pages 部署配置
   // Docker 部署：生成 standalone 输出
   // 注意：Windows 上构建需要管理员权限或在 WSL/Docker 中构建
   // output: 'standalone',
